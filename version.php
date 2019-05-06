5c6f199639dd7f9d5565ed5122a911a6a8dcc265
Benedikt Schneider <benedikt.schneider@hrz.tu-darmstadt.de>
2019-05-06 21:12:58 +0200

    Fix version, typo

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version =  2010050601;
+$plugin->version =  2019050601;
 $plugin->requires = 2010112400; // Moodle 2.0 or above
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = '1.3';
