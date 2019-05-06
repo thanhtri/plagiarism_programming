a96cf414d9158e2bab8c76fbab35084584a1580e
Benedikt Schneider <benedikt.schneider@hrz.tu-darmstadt.de>
2019-05-06 19:37:13 +0200

    Add component name to version.php
    So it is installable in newer versions of Moodle

--- version.php
+++ version.php
@@ -23,8 +23,9 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version =  2014040101;
+$plugin->version =  2010050601;
 $plugin->requires = 2010112400; // Moodle 2.0 or above
-$plugin->maturity = MATURITY_BETA;
+$plugin->maturity = MATURITY_ALPHA;
 $plugin->release = '1.3';
 $plugin->cron     = 7200;
+$plugin->component = 'plagiarism_programming';
